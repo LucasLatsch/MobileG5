17ec275f9a8cce45101a82da7238fc3adf3da06d
joselourencocm <joselourencocm@gmail.com>
2023-11-14 21:51:06 -0300

    Atividade Calcular

---
+++ calculador/App.js
@@ -0,0 +1,5 @@
+import CalculadoraC from "./pages/Home";
+
+export default function App() {
+  return <CalculadoraC />;
+}
---
+++ calculador/pages/Home/index.js
@@ -0,0 +1,134 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  Modal,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+} from "react-native";
+
+export default function CalculadoraC() {
+  const [alcool, setAlcool] = useState("");
+  const [gasolina, setGasolina] = useState("");
+  const [resultado, setResultado] = useState("");
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const calcular = () => {
+    if (alcool && gasolina) {
+      const resultadoCalculo = parseFloat(alcool) / parseFloat(gasolina);
+
+      if (resultadoCalculo < 0.7) {
+        setResultado("Abasteça com álcool!");
+      } else {
+        setResultado("Abasteça com gasolina.");
+      }
+
+      setModalVisible(true);
+    }
+  };
+
+  const calcularNovamente = () => {
+    setAlcool("");
+    setGasolina("");
+    setResultado("");
+    setModalVisible(false);
+  };
+
+  return (
+    <View style={style.container}>
+      <Image source={require("../../assets/bomba.jpg")} style={style.imagem} />
+      <Text style={style.texto}>Qual a melhor opção?</Text>
+      <View style={style.input}>
+        <TextInput
+          style={style.inputs}
+          placeholder="Preço do álcool"
+          keyboardType="numeric"
+          value={alcool}
+          onChangeText={(text) => setAlcool(text)}
+        />
+        <TextInput
+          style={style.inputs}
+          placeholder="Preço da gasolina"
+          keyboardType="numeric"
+          value={gasolina}
+          onChangeText={(text) => setGasolina(text)}
+        />
+      </View>
+      <TouchableOpacity style={style.btn} onPress={calcular}>
+        <Text style={style.textobtn}>Calcular</Text>
+      </TouchableOpacity>
+      <Modal visible={modalVisible} animationType="slide" transparent={true}>
+        <View style={style.containerModal}>
+          <Text style={style.textoModal}>{resultado}</Text>
+          <TouchableOpacity style={style.btn} onPress={calcularNovamente}>
+            <Text style={style.textobtn}>Calcular Novamente</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "lightblue",
+  },
+  inputs: {
+    marginTop: 25,
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 8,
+    padding: 10,
+    width: 170,
+    backgroundColor: "white",
+    fontSize: 18,
+  },
+  input: {
+    marginTop: 25,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 25,
+  },
+  texto: {
+    paddingTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  imagem: {
+    borderRadius: 100,
+    width: 200,
+    height: 200,
+    padding: 40,
+  },
+  btn: {
+    width: 200,
+    backgroundColor: "royalblue",
+    height: 45,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textobtn: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "white",
+  },
+  containerModal: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "rgba(0, 0, 0, 0.9)",
+  },
+  textoModal: {
+    paddingTop: 20,
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "green",
+  },
+});
