b8582d75831a891850762b0060f62437fd54dcbe
LucasLatsch <bello-lucas@hotmail.com>
2023-11-22 21:44:19 -0300

    Splash

---
+++ TrabFinal/pages/Splash/index.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef } from "react";
+import { View, StyleSheet, Button } from "react-native";
+import LottieView from "lottie-react-native";
+
+export default function SplashScreen() {
+  const animation = useRef(null);
+  useEffect(() => {}, []);
+
+  return (
+    <View style={styles.animationContainer}>
+      <LottieView
+        autoPlay
+        ref={animation}
+        style={{
+          width: 200,
+          height: 200,
+          backgroundColor: "#eee",
+        }}
+        source={require("../../assets/assets/Animation_-_1700331134011.json")}
+      />
+      <View style={styles.buttonContainer}>
+        <Button
+          title="Restart Animation"
+          onPress={() => {
+            animation.current?.reset();
+            animation.current?.play();
+          }}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  animationContainer: {
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flex: 1,
+  },
+  buttonContainer: {
+    paddingTop: 20,
+  },
+});
--- TrabFinal/routes/index.js
+++ TrabFinal/routes/index.js
@@ -3,12 +3,14 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Sobre from "../pages/Sobre";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import SplashScreen from "../pages/Splash";
 
 const Drawer = createDrawerNavigator();
 
 export default function AppRouter() {
   return (
     <Drawer.Navigator>
+      <Drawer.Screen name="Animação" component={SplashScreen} />
       <Drawer.Screen
         name="Login"
         component={Login}
